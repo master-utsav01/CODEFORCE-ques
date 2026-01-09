97bc6bdd34efc194ff222d9105885852839b65d9
Utsav <sanjeevkumar@Utsavs-MacBook-Air.local>
2026-01-09 22:05:59 +0530

    122A

--- LuckyDivision.java
+++ LuckyDivision.java
@@ -35,14 +35,24 @@ import java.util.Scanner;
 
 public class LuckyDivision {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        String number = sc.next();
-        for(char ch: number.toCharArray()){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
+        int[] luckyNumbers = {
+                4, 7, 44, 47, 74, 77,
+                444, 447, 474, 477,
+                744, 747, 774, 777
+        };
+
+        boolean isAlmostLucky = false;
+
+        for (int lucky : luckyNumbers) {
+            if (n % lucky == 0) {
+                isAlmostLucky = true;
+                break;
+            }
         }
 
+        System.out.println(isAlmostLucky ? "YES" : "NO");
     }
-
-
-
 }
